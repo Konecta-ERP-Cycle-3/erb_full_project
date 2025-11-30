d8038f1992741e5067667cf49beff3d22caf714a
mhmd <mhmdmstfa710@gmail.com>
2025-12-01 00:32:38 +0200

    fix: Add security group rules for RabbitMQ ports
    - Add ingress rule for RabbitMQ AMQP port 5672
    - Add ingress rule for RabbitMQ Management UI port 15672
    - This allows services to connect to RabbitMQ via service discovery

--- konecta_erp/terraform/ERP/modules/sg/sg.tf
+++ konecta_erp/terraform/ERP/modules/sg/sg.tf
@@ -112,6 +112,22 @@ resource "aws_security_group" "frontend_ecs" {
     description     = "ML model services"
   }
 
+  ingress {
+    from_port       = 5672
+    to_port         = 5672
+    protocol        = "tcp"
+    self            = true
+    description     = "RabbitMQ AMQP"
+  }
+
+  ingress {
+    from_port       = 15672
+    to_port         = 15672
+    protocol        = "tcp"
+    self            = true
+    description     = "RabbitMQ Management UI"
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
